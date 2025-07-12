0bdfb279f679a6ab8f4eba0cf67925f39cde9fc5
kmtusher97 <kmtusher97@gmail.com>
2025-07-12 22:22:06 +0600

    Add counter app basic structure with vite

---
+++ 001-counter-app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1 className="header">Counter App</h1>
+      </div>
+      <div>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
