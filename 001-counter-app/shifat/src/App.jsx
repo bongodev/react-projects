95e3adcc259b4f1afa8db4f84a7754cda9f3f06c
shifat97 <shifatbinreza2@gmail.com>
2025-07-13 22:46:58 +0600

    add: counter app with addtion, subtraction and reset button

---
+++ 001-counter-app/shifat/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./componets/Counter";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-[#EDF4FF]">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
