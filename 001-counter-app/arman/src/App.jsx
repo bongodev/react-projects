7d1c25ed3bcf6dfcfd6ad2c81215ee5b915c992d
armaanepiic <armaanepiicfb17@gmail.com>
2025-07-21 15:08:53 +0600

    react project basic configuration with tailwindcss

---
+++ 001-counter-app/arman/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h1>Counter App</h1>
+      </div>
+      <div>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
