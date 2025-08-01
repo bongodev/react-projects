471c6801f1701981295fd6ac47e8e84ea8e36368
kmtusher97 <kmtusher97@gmail.com>
2025-08-01 22:33:32 +0600

    Add env variables for vite client

--- product-mgmt-app/client/src/config/http.js
+++ product-mgmt-app/client/src/config/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BASE_URL, 
   headers: {
     "Content-Type": "application/json",
   },
